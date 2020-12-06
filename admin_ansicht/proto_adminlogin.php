90c39a222d0904b311ae414290499e72b136d7e0
JoudIsmail <45513984+JoudIsmail@users.noreply.github.com>
2020-12-06 12:19:49 +0100

    Add files via upload

---
+++ admin_ansicht/proto_adminlogin.php
@@ -0,0 +1,26 @@
+<?php
+echo "<!DOCTYPE html>\n";
+echo "<html lang=\"de\">\n";
+echo "	<head>\n";
+echo "		<link rel=\"stylesheet\" type=\"text/css\" href=\"../css/style3.css\"> \n";
+echo "		<link href='https://fonts.googleapis.com/css?family=Poppins' rel=\"stylesheet\">\n";
+echo "		<meta charset=\"UTF-8\">\n";
+echo "		<title> Panel Login - Learnhub </title>\n";
+echo "	</head>\n";
+echo "	<body class=\"admin_body\">\n";
+echo "	\n";
+echo "		<div class=\"box-bg\">\n";
+echo "\n";
+echo "			<div class=\"admin_loginbox\">\n";
+echo "				<h1 class=\"admin_loginbox_element\"> Panel Login </h1>\n";
+echo "				<input class=\"admin_loginbox_element admin_input\" placeholder=\"Benutzername\" /><br>\n";
+echo "				<input type=\"password\" class=\"admin_loginbox_element admin_input\" placeholder=\"Kennwort\"/><br>\n";
+echo "\n";
+echo "				<input class=\"admin_loginbox_element admin_button\" type=\"submit\" value=\"Login\"/>\n";
+echo "			</div>\n";
+echo "\n";
+echo "		</div>\n";
+echo "		\n";
+echo "	</body>\n";
+echo "</html>\n";
+?>
