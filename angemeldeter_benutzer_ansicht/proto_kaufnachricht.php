1729eacf60d91e79e46f554f31d274285cb4efb2
JoudIsmail <45513984+JoudIsmail@users.noreply.github.com>
2020-12-06 12:16:48 +0100

    Add files via upload

---
+++ angemeldeter_benutzer_ansicht/proto_kaufnachricht.php
@@ -0,0 +1,15 @@
+<?php
+echo "<!DOCTYPE html>\n";
+echo "<html lang=\"de\">\n";
+echo "	<head>\n";
+echo "		<link rel=\"stylesheet\" type=\"text/css\" href=\"../css/style3.css\"> \n";
+echo "		<link rel=\"stylesheet\" href='https://fonts.googleapis.com/css?family=Poppins'>\n";
+echo "		<meta charset=\"UTF-8\">\n";
+echo "		<title> Kauf erfolgreich! </title>\n";
+echo "	</head>\n";
+echo "	<body class=\"body_kaufnachricht\">\n";
+echo "		<h1> <span class=\"span-bg\"> Ihr Kauf war erfolgreich! </span> <br> Der Kursleiter wird Ihnen eine E-Mail mit weiteren Anweisungen schreiben. <br>Wir wünschen Ihnen viel Erfolg!</h1>\n";
+echo "		<a  href=\"./proto_gekaufte_kurse.html\">Zu meinen gekauften Kursen</a>\n";
+echo "	</body>\n";
+echo "</html>\n";
+?>
