3bd2b8fbd84818bd7b481503b36829eb312d4b3c
JoudIsmail <45513984+JoudIsmail@users.noreply.github.com>
2020-12-14 00:21:33 +0100

    Update gaestebuch.php

---
+++ gaestebuch.php
@@ -0,0 +1,24 @@
+<?php session_start();
+$page = 'gaestebuch';
+include('../inc/header.php');
+echo "		<div class=\"bewertung-holder\">\n";
+echo "			<h1 class=\"h1bewertung\"> Bewerten Sie unsere Dienstleistungen </h1>\n";
+echo "		</div>\n";
+echo "\n";
+echo "		<a class=\"kerstellung\" id=\"zeige_kommentar_ersteller\" href=\"#\" onClick='(function(){\n";
+echo "			document.getElementById(\"zeige_kommentar_ersteller\").style.visibility = \"hidden\";\n";
+echo "			document.getElementById(\"kommentar_ersteller\").style.visibility = \"visible\";\n";
+echo "		})(); return false;'>Neuen Eintrag verfassen</a>\n";
+echo "		<div class=\"kommentarersteller\" id=\"kommentar_ersteller\">\n";
+echo "			<form action=\"do.php\" method= \"post\">\n";
+echo "				<input name=\"title\" placeholder=\"Titel\" /><br>\n";
+echo "				<textarea name=\"comment\" class=\"comment\" placeholder=\"Kommentar\"></textarea><br>\n";
+echo "				<input name=\"name\" placeholder=\"Name\" /><br>\n";
+echo "				<input name=\"addEntry\" type=\"submit\" value=\"Senden\" />\n";
+echo "			</form>\n";
+echo "		</div>\n";
+echo "		<div class=\"comment-container\">\n";
+generateEntries(5,0);
+echo "		</div>\n";
+include('../inc/footer.php');
+?>
