f45d0ce44e64bfb189aa9856e09e8f7c9d691abf
JoudIsmail <45513984+JoudIsmail@users.noreply.github.com>
2020-12-13 19:59:23 +0100

    Update sekundaerfunktionen.php

--- lib/sekundaerfunktionen.php
+++ lib/sekundaerfunktionen.php
@@ -14,28 +14,48 @@
 		}
 	}
 
+		/*shop courses to do */
+
 	function generateShopCourses($amount, $offset){
 		$courses = readCoursesFile();
 		$subcourses = array_slice($courses, $offset, $amount);
-		$outputHTML = „“
-		if $amount == $courses{
-			for ($i = 1; $i <= $subcourses; $i++) {
-				$outputHTML = $outputHTML + $subcourses[i];
+		if ($amount == $courses){
+			foreach($subcourses as $course){
+				echo "                <div class=\"columns column-66\">\n";
+				echo "                    <div class=\"col-21\">\n";
+				echo "                        <a class=\"kurs_link_EK\" href=\"kurs.php?id=".$course["id"]."\">\n";
+				echo "                            <img class=\"kurs_box_EK\" alt=".$course["courseImageAlt"]." src=".$course["courseImagePath"]."></a>\n";
+				echo "                    </div>\n";
+				echo "                    <div class=\"col-69\">\n";
+				echo "                            <a class=\"p-420\" href=\"kurs.php?id=".$course["id"]."\"> <h3> <strong> <u> ".$course["title"]." </u> </strong> </h3> \n";
+				$course['summary'];
+				echo "                                                    </a>\n";
+				echo "                    </div>\n";
+				echo "                    <div class=\"col-420\"><p class=\"p-bg\">\n";
+				echo "                        Ersteller:".$course['instructorName']." <br>\n";
+				echo "                                                Preis:".$course['priceEuro'].",".$course['priceCent']."€</p><br>\n";
+				echo "						<form action=\"do.php\">\n";
+				echo "                                                <input name=\"buyCourse\" type=\"submit\" class=\"btn-purchase\" value=\"Kurs kaufen\">\n";
+				echo "						<input type=\"hidden\" name=\"id\" value =\"".$course['id']."\">\n";
+				echo "						</form>\n";
+				echo "                    </div>\n";
+				echo "                </div>\n";
 			}
-			
-			echo $outputHTML;
 		}
 	}
 
+	/*bought courses to do */
+
 	function generateBoughtCourses(){
 		$courses = readCoursesFile();
-		$outputHTML = „“;
-		$course =  $_SESSION[„boughtCourses“];
-		for ($i = 1; $i <= $course; $i++) {
-			$outputHTML = $outputHTML + $course;
-		}
-			
-		echo $outputHTML;	
+		foreach ($_SESSION["boughtCourses"] as $course){
+			echo "			<div class=\"grid_container_EK\">\n";
+			echo "				<div class=\"grid_element_EK1\">\n";
+			echo "					<a href=\"kurs.php?id=".$course["id"]."\"> <img alt=".$course["courseImageAlt"]." class=\"grid_element_bild_EK\" src=".$course["courseImagePath"]."> </a>\n";
+			echo "					<h3 class=\"grid_element_titel_EK\" >".$course["title"]."</h3>\n";
+			echo "				</div>\n";
+			echo "	\n";
+		}	
 	}
 	
 	function generateParticipantsList($kursId){
