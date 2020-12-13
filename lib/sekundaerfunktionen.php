5da5641fc6f3ff8c86377b1bdae0549debee6730
mtrinkaus01 <73189447+mtrinkaus01@users.noreply.github.com>
2020-12-14 00:14:39 +0100

    Update sekundaerfunktionen.php

--- lib/sekundaerfunktionen.php
+++ lib/sekundaerfunktionen.php
@@ -58,31 +58,44 @@
 		}	
 	}
 	
+	function generateCreatedCourses(){
+		$course = $courses = readCoursesFile();
+		foreach($_SESSION["createdCourses"] as $course){
+			echo "				<div class=\"grid_element_EK1\">\n";
+			echo "					<a href=\"kurs.php?id=".$course["id"]."\"> <img alt=".$course["courseImageAlt"]." class=\"grid_element_bild_EK\" src=".$course["courseImagePath"]."> </a>\n";
+			echo "					<h3 class=\"grid_element_titel_EK\" >".$course["title"]."</h3>\n";
+			echo "				</div>\n";
+			echo "	\n";
+		}
+	}
+
 	function generateParticipantsList($kursId){
-		$course = $_SESSION[„createdCourses“][„kursId“];
+		$course = $_SESSION["createdCourses"]["kursId"];
 		$emailArray = array();
 		$users = readUsersFile();
-		for ($i = 1; $1 <= $course[„participants“]; $i++){
+		foreach ($course["participants"] as $participant){
 			$user = findUserById($users, $userid);
-			array_push($emailArray, $user[„email“]);
+			array_push($emailArray, $user["email"]);
 		}
-		$outputHTML = „“;
 		$emailArraySize = sizeof($emailArray);
-		for ($i = 1; $i <= $emailArraySize; $i++){
-			$outputHTML = $outputHTML + $emailArray[i];
+		foreach($emailArray as $email){
+			echo "                    <p class=\"p-bg\"> <strong>".$email["email"]." </strong></p>\n";
 		}
-		echo $outputHTML;
 	}
 	
 	function generateEntries($amount, $offset){
 		$entries = readEntriesFile();
 		$subentries = array_slice($entries, $offset, $amount);
-		$outputHTML = „“;
 		$subentriesSize = sizeof($subentries);
-		for ($i = 1; $i <= $subentriesSize; $i++){
-			$outputHTML = $outputHTML + $subentries[i];
+		foreach($subentries as $entry){
+			echo "			<div class=\"kommentar_box_GB\">\n";
+			echo "				<h3>".$entry["title"]."</h3>\n";
+			echo "				<p class=\"kommentar_text_GB\">".$entry["comment"]."</p>\n";
+			echo "				<p class=\"kommentar_info_GB\">".$entry["name"]."</p>\n";
+			echo "				<p class=\"kommentar_info_GB\">".$entry["dateTimeString"]."</p>\n";
+			echo "			</div>\n";
+			echo "			<hr class=\"kommentar_seperator_GB\">\n";
 		}
-		echo $outputHTML;
 	}
 
 ?>
